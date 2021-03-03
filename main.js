3e0269f86abf93cfe8325c7fa3f6398e5607aa05
strongstarter <43767525+strongstarter@users.noreply.github.com>
2021-03-03 13:32:07 +0900

    Item_random deployment

--- main.js
+++ main.js
@@ -1,1 +1,38 @@
 'use strict'
+const field = document.querySelector('.game__field');
+const fieldRect = field.getBoundingClientRect();
+const CARROT_SIZE = 80;
+const CARROT_COUNT = 5;
+const BUG_COUNT = 5;
+
+function initGame() {
+    //벌레와 당근을 생성한 뒤 field에 추가해줌
+    console.log(fieldRect);
+    addItem('carrot', CARROT_COUNT, 'img/carrot.png');
+    addItem('bug', BUG_COUNT, 'img/bug.png');
+}
+
+function addItem(className, count, imgPath) {
+    const x1 = 0;
+    const y1 = 0;
+    const x2 = fieldRect.width - CARROT_SIZE;
+    const y2 = fieldRect.height - CARROT_SIZE;
+    for (let i = 0; i < count; i++) {
+        const item = document.createElement('img');
+        item.setAttribute('class', className);
+        item.setAttribute('src', imgPath);
+        item.style.position = 'absolute';
+        const x = randomNumber(x1, x2);
+        const y = randomNumber(y1, y2);
+        item.style.left = `${x}px`;
+        item.style.top = `${y}px`
+        field.appendChild(item);
+    }
+}
+
+function randomNumber(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+
+initGame();
